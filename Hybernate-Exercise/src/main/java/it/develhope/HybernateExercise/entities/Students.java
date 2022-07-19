b2c9f1dbc985894f5a1a5b5e4c4a94688f34e496
AntonioQuatrale93 <103583055+AntonioQuatrale93@users.noreply.github.com>
2022-07-19 12:27:26 +0200

    Add files via upload

---
+++ Hybernate-Exercise/src/main/java/it/develhope/HybernateExercise/entities/Students.java
@@ -0,0 +1,26 @@
+package it.develhope.HybernateExercise.entities;
+
+import com.sun.istack.NotNull;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Data
+public class Students {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @NotNull
+    @Column(nullable = false)
+    private String lastname;
+    @NotNull
+    @Column(nullable = false)
+    private String firstname;
+    @NotNull
+    @Column(nullable = false, unique = true)
+    private String email;
+
+
+}
